e07b401200c1d9a760617fdfd7f5be24452c4970
Valeriya Dudchenko <vvd.2209@bk.ru>
2023-08-26 13:19:42 +0300

    Initial Class Channel

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,29 @@
+import json
+import os
 
+from googleapiclient.discovery import build
+
+import isodate
+
+api_key: str = 'AIzaSyD_wRK3rMN3ZOogw_aXisLVn3mjnCLX46M'
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        printj(channel)
+
